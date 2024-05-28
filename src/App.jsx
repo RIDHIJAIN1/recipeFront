9b4c750c8b241b8b3c382e68b3a17a4c793a2ae3
Ridhi <Ridhi@gonubordiya749@gmail.com>
2024-05-28 19:29:52 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,49 @@
-import React, { useEffect } from 'react'
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Home.jsx'
-// import '../src/styles/app.scss'
-import Recipe from './Recipe.jsx'
-import Single from './Single.jsx'
-import Login from './Login.jsx'
-import Signup from './Signup.jsx'
-import Admin from './Admin.jsx'
-
-import { Toaster } from 'react-hot-toast'
-import Footer from './Footer.jsx'
-import axios from 'axios'
-import Context, { server } from './main.jsx'
-import { useContext } from 'react'
-
+import React, { useEffect, useContext } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home.jsx';
+import Recipe from './Recipe.jsx';
+import Single from './Single.jsx';
+import Login from './Login.jsx';
+import Signup from './Signup.jsx';
+import Admin from './Admin.jsx';
+import Footer from './Footer.jsx';
+import { Toaster } from 'react-hot-toast';
+import axios from 'axios';
+import Context, { server } from './main.jsx';
 
 const App = () => {
+  const { setUser, setIsAuthenticated, setLoading } = useContext(Context);
 
-  const{setUser , setIsAuthenticated , setLoading} = useContext(Context)
-
-  useEffect(()=>{
+  useEffect(() => {
     setLoading(true);
-
-    axios.get(`${server}/users/me`,{
-      withCredentials:true,
-    }).then(res=>{
+    axios.get(`${server}/users/me`, {
+      withCredentials: true,
+    }).then(res => {
       setUser(res.data.user);
       setIsAuthenticated(true);
       setLoading(false);
-
-    }).catch((error)=>{
-       setUser({})
-       setIsAuthenticated(false);
-       setLoading(false);
-    })
-    
-    },[3000]);
+    }).catch(error => {
+      console.error('Error fetching user data:', error); // Add detailed error logging
+      setUser({});
+      setIsAuthenticated(false);
+      setLoading(false);
+    });
+  }, [setUser, setIsAuthenticated, setLoading]);
 
   return (
-    <Router> 
-         
-    <Routes>
-    <Route path='/'element={<Home/>}/>
-    <Route path='/recipe'element={<Recipe/>}/>
-    {/* <Route path='/single'element={<Single/>}/> */}
-    <Route path='/login'element={<Login/>}/>
-    <Route path='/signup'element={<Signup/>}/>
-    <Route path='/admin'element={<Admin/>}/>
-    <Route path='/footer'element={<Footer/>}/>
-    <Route path="/recipe/:id" element={<Single/>} />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/recipe' element={<Recipe />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/footer' element={<Footer />} />
+        <Route path="/recipe/:id" element={<Single />} />
+      </Routes>
+      <Toaster />
+    </Router>
+  );
+};
 
-    
-    </Routes>
-    <Toaster/>
-  </Router>
-  )
-}
-
-export default App
+export default App;
